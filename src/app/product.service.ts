cda6931cd9b129eb3ef949e565cde254bb3fd340
Adsule <dharmendra.adsule1@t-mobile.com>
2018-10-13 23:49:27 -0700

    new changes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import {Album} from './album';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
 
-  public getAlbum (id: number) {
+  public getAlbum: Observable<Album> (id: number) {
     return this._http.get(this._albumUrl).map(value => value.json());
   }
 
